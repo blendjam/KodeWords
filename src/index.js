58da6e1355608558839ed4ab2a881a4589ae99d7
llsanketll <slamsal2000@gmail.com>
2025-02-04 14:34:14 +0545

    bg

--- src/index.js
+++ src/index.js
@@ -14,8 +14,6 @@ const router = createHashRouter([
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <RouterProvider
-    router={router}
-  >
+  <RouterProvider router={router} >
   </RouterProvider>
 );
