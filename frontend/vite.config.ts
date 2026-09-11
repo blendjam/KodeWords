0d5a00ac50f471d099a50cda059484d5de680ba3
blendjam <slamsal2000@gmail.com>
2026-09-11 22:30:20 +0545

    fix: deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/KodeWords/",
+  base: "/kodewords/",
   build: {
     outDir: "build",
   },
